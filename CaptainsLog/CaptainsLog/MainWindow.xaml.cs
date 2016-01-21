c7c8d3e5c4377ef29ae923306564ab105f749582
Rebecca Mooney <astrakiseki@gmail.com>
2016-01-21 08:56:07 -0800

    In progress

---
+++ CaptainsLog/CaptainsLog/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CaptainsLog.Core;
+using System.Collections.ObjectModel;
+
+namespace CaptainsLog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<LogEntry> logEntries;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            logEntries = new ObservableCollection<LogEntry>();
+            LogEntries.ItemsSource = logEntries;
+        }
+
+        private void AddEntry_Click(object sender, RoutedEventArgs e)
+        {
+            // your event handler, created for you by Visual Studio
+            // instantiate an  instance of DetailWindow
+            TextWindow detailWindow = new TextWindow();
+
+            // it's not enough to instantiate, let's actually open the window.
+            detailWindow.Show();
+
+        }
+
+        private void EditEntry_Click(object sender, RoutedEventArgs e)
+        {
+            // for editing entries
+        }
+
+        private void DeleteEntry_Click(object sender, RoutedEventArgs e)
+        {
+            // for deleting entries, need to figure out how to select an entry...
+
+            Input 
+
+        }
+
+    }
+    }
+
+
---
+++ CaptainsLog/CaptainsLog/TextWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using CaptainsLog.Core;
+
+
+namespace CaptainsLog
+{
+    /// <summary>
+    /// Interaction logic for TextWindow.xaml
+    /// </summary>
+    public partial class TextWindow : Window
+    {
+
+        public TextWindow()
+        {
+            InitializeComponent();
+            // collect the input from the user
+            // validate the info (did they add enough text)
+            // add to grid
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox.Text;
+
+            LogEntry log = new LogEntry();
+            string title = Title.Text;
+
+            log.Id = MainWindow.logEntries.Count + 1;
+            log.Title = title;
+            log.Text = text;
+            log.EntryDate = DateTime.Now;
+
+           MainWindow.logEntries.Add(log);
+            Close();
+        }
+    }
+}
+
